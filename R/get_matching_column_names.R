5dedffb6479e595572fd272bdc80e4e8a63765d4
Nikita Shymberg <nikitashymberg@gmail.com>
2022-01-25 15:50:45 -0800

    working on implementation

--- R/get_matching_column_names.R
+++ R/get_matching_column_names.R
@@ -13,7 +13,27 @@
 #' data <- data.frame(week_payment1 = c(1, 2, 3), week_payment2 = c(1, 2, 3),
 #'                    week_payment3 = c(1, 2, 3), othercolumn = c(5, 6, 7))
 #' get_matching_column_names(data, "week_payment")
+library(stringr)
 
 get_matching_column_names <- function(df, pattern) {
-    NULL
+    if(!is.data.frame(df)){
+        stop("The df variable needs to be a dataframe")
+    }
+    if(!is.character(pattern)){
+        stop("The pattern variable needs to be a character type")
+    }
+    
+    pattern <- paste0(pattern, "[0-9]+")
+    matches <- colnames(df) |>
+        str_match(pattern) |>
+        is.na() |>
+        isFALSE()  # TODO: I think this is wrong fix me next time
+    
+    matches
+    
+    # if(!any(matches)){
+    #     stop("No columns matched the given pattern")
+    # }
+    # 
+    # colnames(df)[matches]
 }
